2219cfc39ed89ce7bc59b8300ca0b7265b38291a
Carlos Alves <carlosalves369m@hotmail.com>
2022-06-11 15:41:27 -0300

    update `hashes.rb`

--- algorithm/hashes.rb
+++ algorithm/hashes.rb
@@ -7,3 +7,43 @@ states = {
 # puts states
 # puts states["Oregon"]
 puts states[:Oregon]
+
+
+
+# Another way to use hashes
+
+balances = {
+  'Carlos' => 1000,
+  'Matheus' => 1200,
+  'Pamela' => 1500,
+  'Claudio' => 1700,
+  'Vanessa' => 2200
+}
+
+balances.each_pair do |name, balance|
+  puts "#{name} tem R$#{balance}"
+end
+
+
+sum = 0
+
+balances.each_pair do |name, balance|
+  sum+=balance
+end
+puts "___________________________"
+puts "TOTAL = #{sum}"
+
+
+
+
+# Sorting hashes
+
+hash = {
+  3 => 'c',
+  1 => 'a',
+  2 => 'b'
+}
+
+hash.keys.sort.each do |key|
+  puts "#{key} : #{hash[key]}"
+end
